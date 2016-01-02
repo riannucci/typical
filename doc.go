32b5d324c312546d160a0e586793781e276d17f9
Robert Iannucci <robbie@rail.com>
2016-01-02 04:23:51 -0800

    Number actually improved with the removal of that closure

--- doc.go
+++ doc.go
@@ -8,7 +8,7 @@
 //
 // Not as bad as I initially suspected! There's a very trivial set of benchmarks
 // which are fun to profile and try to optimize. Initially the Typical one ran
-// at ~20000ns/op, but now runs at ~8500ns/op on the same hardware. The
+// at ~20000ns/op, but now runs at ~8000ns/op on the same hardware. The
 // conventional implementation takes ~4000ns/op. Allocation is also not
 // horrible; currently `976 B/op   26 allocs/op` vs `504 B/op   10 allocs/op`
 // (with go 1.4.2 on linux).
